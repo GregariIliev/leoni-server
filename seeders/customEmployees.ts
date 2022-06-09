50f7af022855516ef6cc49ab82b0a3eff157244c
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-09 19:09:44 +0300

    create cunstom employees

---
+++ seeders/customEmployees.ts
@@ -0,0 +1,24 @@
+export const employees = [
+    {
+        firstName: 'Gregari',
+        middleName: 'Georgiev',
+        lastName: 'Iliev',
+        address: 'Drujba 427',
+        phone: '0885744009',
+        salary: 1000,
+        shift: 'A',
+        departmentId: 1,
+        adminId: 1
+    },
+    {
+        firstName: 'Marina',
+        middleName: 'Nikolaeva',
+        lastName: 'Ilieva',
+        address: 'Drujba 427',
+        phone: '0876542245',
+        salary: 1000,
+        shift: 'A',
+        departmentId: 1,
+        adminId: 2
+    }
+]
