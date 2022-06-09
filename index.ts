3b63a42d033fdee8032264b31d529aeec2ba9546
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-09 14:22:52 +0300

    create database whit mysql query

--- index.ts
+++ index.ts
@@ -7,3 +7,14 @@ import db from './models';
 
 const port = process.env.SERVER_PORT || 3000;
 const app = express();
+
+mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
+    .then(async (c) => {
+        await c.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME};`);
+        await c.end();
+    })
+    .catch(err => console.log(err));
