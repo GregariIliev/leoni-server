5f94befa21f789598509edfc23429593a9e436ad
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-09 19:10:02 +0300

    create custom positions

---
+++ seeders/customPositions.ts
@@ -0,0 +1,22 @@
+export const positions = [
+    {
+        name: 'Machine Operator',
+        salaryMultiplayer: 1.2
+    },
+    {
+        name: 'Quality Inspector',
+        salaryMultiplayer: 1.5
+    },
+    {
+        name: 'Shift Leader',
+        salaryMultiplayer: 1.8
+    },
+    {
+        name: 'Maintenance',
+        salaryMultiplayer: 1.6
+    },
+    {
+        name: 'Trainer',
+        salaryMultiplayer: 1.4
+    }
+]
