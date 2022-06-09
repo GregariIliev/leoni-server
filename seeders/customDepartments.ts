a2ffbf8ce39cbf0cf62d8f03e3e4429c05f5acb3
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-09 19:09:35 +0300

    create cunstom departments

---
+++ seeders/customDepartments.ts
@@ -0,0 +1,22 @@
+export const departments = [
+    {
+        name: 'Cutting',
+        maxEmployees: 20,
+        salaryMultiplayer: 1.2
+    },
+    {
+        name: 'WPA',
+        maxEmployees: 20,
+        salaryMultiplayer: 1.1
+    },
+    {
+        name: 'Engine',
+        maxEmployees: 20,
+        salaryMultiplayer: 1.8
+    },
+    {
+        name: 'HR',
+        maxEmployees: 20,
+        salaryMultiplayer: 1.5
+    }
+]
