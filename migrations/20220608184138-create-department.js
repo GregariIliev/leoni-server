4bcff1dd4229c88f149238ef9b00333af69403fa
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-09 14:18:29 +0300

    create migrations whit sequelize-cli

---
+++ migrations/20220608184138-create-department.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Departments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      maxEmployees: {
+        type: Sequelize.INTEGER
+      },
+      salaryMultiplayer: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Departments');
+  }
+};
---
+++ migrations/20220608185442-create-employee.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fistName: {
+        type: Sequelize.STRING
+      },
+      middleName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      salary: {
+        type: Sequelize.NUMBER
+      },
+      shift: {
+        type: Sequelize.STRING
+      },
+      departmentId: {
+        type: Sequelize.NUMBER
+      },
+      positionId: {
+        type: Sequelize.NUMBER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Employees');
+  }
+};
