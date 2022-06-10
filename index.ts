07f69ad5c9378d147f446cb9a56ccc2d49d6a64b
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-10 20:50:49 +0300

    print on constole database connected

--- index.ts
+++ index.ts
@@ -22,11 +22,14 @@ mysql.createConnection({
         await c.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME};`);
         await c.end();
 
-        db.sequelize.sync({ force: true }).then(() => {
+        db.sequelize.sync({force: true}).then(() => {
 
             employees.map(async (e) => await db.Employee.create(e));
             departments.map(async (d) => await db.Department.create(d));
             positions.map(async (p) => await db.Position.create(p));
+
+            console.log("Database connected successful.");
+            
 
             const app = new App();
 
