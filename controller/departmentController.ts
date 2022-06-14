85c01f156584a0344bea6fbcb0fc623aa16ff6cc
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-15 01:52:21 +0300

    rename controller

--- controller/departmentController.ts
+++ controller/departmentController.ts
@@ -2,19 +2,21 @@ import { Router } from "express";
 
 import { DepartmentService } from "../service/departmentService";
 
-export class DashboardController {
-    declare router: Router;
+export class DepartmentController {
     declare departmentService: DepartmentService;
 
-    constructor() {
-        this.router = Router();
+    constructor(private router: Router) {
         this.departmentService = new DepartmentService();
     }
 
     setRoutes() {
         this.router.get('/api/departments', async (req: any, res: any) => {
             try {
-                const departments = await this.departmentService.getAllDepartments();
+
+                console.log(req.query);
+                
+
+                const departments = await this.departmentService.getAllDepartmentsIncludePositions();
 
                 if (!departments) {
                     throw new Error('Fetch departments fail!')
@@ -23,7 +25,7 @@ export class DashboardController {
                 res.status(200).json(departments);
 
             } catch (err) {
-                res.status(404).json({ Error: 'Fetch departments fail!' });
+                res.status(404).send();
             }
         })
     }
