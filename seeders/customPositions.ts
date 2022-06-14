169f4bb5c88f4e2eea99f7a31ef10a1adbcfef3c
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-15 02:14:00 +0300

    add shifts on custom positions

--- seeders/customPositions.ts
+++ seeders/customPositions.ts
@@ -1,22 +1,28 @@
 export const positions = [
     {
         name: 'Machine Operator',
-        salaryMultiplayer: 1.2
+        salaryMultiplayer: 1.2,
+        shift: 'A, B, C'
     },
     {
         name: 'Quality Inspector',
-        salaryMultiplayer: 1.5
+        salaryMultiplayer: 1.5,
+        shift: 'A, B, C'
     },
     {
         name: 'Shift Leader',
-        salaryMultiplayer: 1.8
+        salaryMultiplayer: 1.8,
+        shift: 'A, B, C'
     },
     {
         name: 'Maintenance',
-        salaryMultiplayer: 1.6
+        salaryMultiplayer: 1.6,
+        shift: 'A, B, C'
     },
     {
         name: 'Trainer',
-        salaryMultiplayer: 1.4
+        salaryMultiplayer: 1.4,
+        shift: 'A, B, C'
+    },
     }
 ]
