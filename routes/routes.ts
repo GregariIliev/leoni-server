80ecfa7621a87d7e7d30762f07ec8aaa041209b1
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-15 13:34:49 +0300

    rename authController to employeeController

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,28 +1,28 @@
 import { Router } from "express";
 
-import { AuthController } from "../controller/authController";
+import { EmployeeController } from "../controller/employeeController";
 import { DepartmentController } from "../controller/departmentController";
 import { PositionController } from "../controller/positionController";
 
 export class Routes {
     declare router: any;
-    declare authController: AuthController;
+    declare employeeController: EmployeeController;
     declare departmentController: DepartmentController;
     declare positionController: PositionController;
 
     constructor() {
         this.router = Router();
-        this.authController = new AuthController(this.router);
+        this.employeeController = new EmployeeController(this.router);
         this.departmentController = new DepartmentController(this.router);
         this.positionController = new PositionController(this.router);
     }
 
     setRoutes() {
-        this.authController.setRoutes();
+        this.employeeController.setRoutes();
         this.departmentController.setRoutes();
         this.positionController.setRoutes();
 
-        this.router.use(this.authController.getRoutes());
+        this.router.use(this.employeeController.getRoutes());
         this.router.use(this.departmentController.getRoutes());
         this.router.use(this.positionController.getRoutes());
     }
