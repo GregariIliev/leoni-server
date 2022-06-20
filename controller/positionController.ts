ebf5fcd73a9e4d9b82d47cfc914d781dcded0a24
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-20 21:05:45 +0300

    create get all and count functions

--- controller/positionController.ts
+++ controller/positionController.ts
@@ -7,9 +7,45 @@ export class PositionController {
     constructor(private router: Router, private positionService: PositionService) { }
 
     setRoutes() {
-        // this.router.get('/api/positions', async (req: any, res: any) => {
-        //     try {
-        //         const positions = await this.positionService.getAllPositions();
+        this.getAll();
+        this.getCount();
+        this.create();
+    }
+
+    getAll() {
+        this.router.get('/api/positions', async (req: Request, res: Response) => {
+            try {
+                const positions = await this.positionService.getAll();
+
+                if (!positions) {
+                    throw new Error('Fetch positions fail!');
+                }
+
+                res.status(200).json(positions);
+
+            } catch (err) {
+                res.status(404).send(err);
+            }
+
+        })
+    }
+
+    getCount() {
+        this.router.get('/api/positions/count', async (req: Request, res: Response) => {
+            try {
+                const positionsCount = await this.positionService.count();
+
+                if (!positionsCount) {
+                    throw new Error('Fetch positions count fail.');
+                }
+
+                res.status(200).json(positionsCount);
+
+            } catch (err) {
+                res.status(404).send(err);
+            }
+        })
+    }
 
         //         if (!positions) {
         //             throw new Error('Fetch positions fail!');
