93f19342c62c2bcbc656b85a003bfa000c4b2468
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-21 01:00:13 +0300

    create middleware to chek token

---
+++ middleware/authentication.ts
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+import { Request, Response, NextFunction } from 'express';
+
+
+export const valid = (req: Request, res: Response, next: NextFunction) => {
+    if (req.url === '/api/employees/login') {
+        next();
+
+    } else if (req.headers.cookie) {
+
+        const token: any = req.headers.cookie?.split('=')[1];
+
+        const secret: any = process.env.JWT_SECRET;
+
+        const valid = jwt.verify(token, secret)
+
+        if (valid) {
+            next();
+            
+        } else {
+            res.status(401).json({ unauthorized: true });
+        }
+    } else {
+        res.status(401).json({ unauthorized: true });
+    }
+}
