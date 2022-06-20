ab1d9faf7c5bc553a83c3a4ac24537c549684cac
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-20 21:01:03 +0300

    inject all services to controllers

--- routes/routes.ts
+++ routes/routes.ts
@@ -4,17 +4,25 @@ import { EmployeeController } from "../controller/employeeController";
 import { DepartmentController } from "../controller/departmentController";
 import { PositionController } from "../controller/positionController";
 
+import { EmployeeService } from "../service/employeeService";
+import { DepartmentService } from "../service/departmentService";
+import { PositionService } from "../service/positionService";
+
 export class Routes {
     declare router: any;
     declare employeeController: EmployeeController;
     declare departmentController: DepartmentController;
     declare positionController: PositionController;
 
+    employeeService: EmployeeService = new EmployeeService();
+    departmentService: DepartmentService = new DepartmentService();
+    positionService: PositionService = new PositionService();
+
     constructor() {
         this.router = Router();
-        this.employeeController = new EmployeeController(this.router);
-        this.departmentController = new DepartmentController(this.router);
-        this.positionController = new PositionController(this.router);
+        this.employeeController = new EmployeeController(this.router, this.employeeService, this.departmentService, this.positionService);
+        this.departmentController = new DepartmentController(this.router, this.departmentService);
+        this.positionController = new PositionController(this.router, this.positionService);
     }
 
     setRoutes() {
