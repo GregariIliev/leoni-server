7f6409ff1724f9b752b4b30aac01680d3a5dcb51
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-24 16:17:07 +0300

    substitute first check cookie name leoni to check url login path

--- middleware/authentication.ts
+++ middleware/authentication.ts
@@ -4,10 +4,10 @@ import { Request, Response, NextFunction } from 'express';
 
 
 export const authenticate = (req: Request, res: Response, next: NextFunction) => {
-
-    if (req.headers.cookie?.split('=')[0] === 'leoni') {
-
-        try {
+    try {
+        if (req.url === '/api/employees/login') {
+            next();
+        } else {
             const token: any = req.headers.cookie?.split('=')[1];
             const secret: any = process.env.JWT_SECRET;
             const valid = jwt.verify(token, secret)
