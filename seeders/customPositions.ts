d34f613c2ed06947b339e6f5a5a89d0dae0291a1
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-29 21:56:23 +0300

    remove spaces in the shits string

--- seeders/customPositions.ts
+++ seeders/customPositions.ts
@@ -2,27 +2,27 @@ export const positions = [
     {
         name: 'Machine Operator',
         salaryMultiplayer: 1.2,
-        shift: 'A, B, C'
+        shift: 'A,B,C'
     },
     {
         name: 'Quality Inspector',
         salaryMultiplayer: 1.5,
-        shift: 'A, B, C'
+        shift: 'A,B,C'
     },
     {
         name: 'Shift Leader',
         salaryMultiplayer: 1.8,
-        shift: 'A, B, C'
+        shift: 'A,B,C'
     },
     {
         name: 'Maintenance',
         salaryMultiplayer: 1.6,
-        shift: 'A, B, C'
+        shift: 'A,B,C'
     },
     {
         name: 'Trainer',
         salaryMultiplayer: 1.4,
-        shift: 'A, B, C'
+        shift: 'A,B,C'
     },
     {
         name: 'Development Engineer',
