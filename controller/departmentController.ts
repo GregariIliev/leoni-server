26d09033a50b6e4f1043b3b66c770db3ca8cb54a
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-29 21:43:19 +0300

    create getById, getUpdate and getDelete routes

--- controller/departmentController.ts
+++ controller/departmentController.ts
@@ -11,6 +11,27 @@ export class DepartmentController {
         this.getCount();
         this.getDepartmentsPositions();
         this.create();
+        this.getById();
+        this.getUpdate();
+        this.getDelete();
+    }
+
+    getById() {
+        this.router.get('/api/departments/:id', async (req: Request, res: Response) => {
+            try {
+                const id = req.params.id;
+
+                const department = await this.departmentService.getById(id);
+
+                if (!department) {
+                    throw new Error(`Cannot find departmanet whit id ${id}`);
+                }
+                res.status(201).json(department);
+
+            } catch (err) {
+                res.status(404).json(err);
+            }
+        })
     }
 
     getAll() {
@@ -67,16 +88,48 @@ export class DepartmentController {
     create() {
         this.router.post('/api/departments', async (req: Request, res: Response) => {
             try {
-                //verify cookie
+                const { positions } = req.body;
 
                 const department = await this.departmentService.create(req.body);
+                await department.addPositions(positions);
 
                 if (!department) {
                     throw new Error('Fail to create new department');
                 }
-
+                res.status(201).json(department)
             } catch (err: any) {
                 res.status(424).send(err);
+            }
+        })
+    }
+
+    getUpdate() {
+        this.router.put('/api/departments/:id', async (req: Request, res: Response) => {
+            try {
+                const departmentId = req.params.id;
+                const department = req.body;
+
+                const id = await this.departmentService.update(department, departmentId);
+
+                res.status(201).json('id');
+            } catch (err) {
+                console.log(err);
+
+                res.status(404).send();
+            }
+        })
+    }
+
+    getDelete() {
+        this.router.delete(`/api/departments/:id`, async (req: Request, res: Response) => {
+            try {
+                const id = req.params.id;
+
+                const deleted = await this.departmentService.delete(id);
+
+                res.status(200).json(deleted)
+            } catch (err) {
+                res.status(404).json(err);
             }
         })
     }
